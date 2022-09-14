3162a4f16d8a86b9655ab2ff11a7a8ee6104c9f0
Gustavo Oliveira Quinteiro <gustavo.quinteiro@ufba.br>
2022-09-14 10:45:55 -0300

    add auth module

---
+++ src/auth/constants.ts
@@ -0,0 +1,9 @@
+require('dotenv').config();
+export const jwtConstants = {
+  secretKey: process.env.SECRET_KEY,
+  expirationTime: process.env.EXPIRES_IN, 
+}
+export const errorMessages = {
+  userNotFoundMessage: "User not found",
+  invalidPasswordMessage: "Invalid password",
+}
