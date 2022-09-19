760e125c8ef45f5b83305851d0801e454b130bea
Breno Rosas <breno.rosas@reidopitaco.com>
2022-09-18 22:34:42 -0300

    feat: add token to recovery password email

--- src/password-recovery/service/password-recovery.service.ts
+++ src/password-recovery/service/password-recovery.service.ts
@@ -4,26 +4,31 @@ import { EmailService } from '../../email/service/email.service'
 import { AdvisorService } from '../../advisor/service/advisor.service'
 import { StudentsService } from '../../students/service/students.service'
 import { PasswordRecoveryRequestDto } from '../ model/password-recovery-request.dto'
+import { ConfigService } from '@nestjs/config'
+import { JwtService } from '@nestjs/jwt'
 
 @Injectable()
 export class PasswordRecoveryService {
   constructor(
     private studentsService: StudentsService,
     private emailService: EmailService,
-    private advisorService: AdvisorService
+    private advisorService: AdvisorService,
+    private configService: ConfigService,
+    private readonly jwtService: JwtService
   ) {}
 
   async requestPasswordRecovery(
     dto: PasswordRecoveryRequestDto
   ): Promise<void> {
+    const payload = { email: dto.email }
+    const token = this.jwtService.sign(payload)
     await this.emailService.sendEmail({
       to: dto.email,
       template: 'recovery-password-request',
-      from: 'noreply@application.com',
       context: {
-        token: dto.email
+        resetPasswordUrl: this.configService.get('RESET_PASSWORD_URL'),
+        token
       }
     })
-    console.log(dto)
   }
 }
