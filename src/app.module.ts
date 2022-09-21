ee07732cd860f975f1ea10691ec96e4bec9f0299
Bruno Barbosa <60590932+brunodelucasbarbosa@users.noreply.github.com>
2022-09-20 21:55:30 -0300

    Update app.module.ts
    Signed-off-by: Bruno Barbosa <60590932+brunodelucasbarbosa@users.noreply.github.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { DatabaseModule } from './database/database.module'
+//import { DatabaseModule } from './database/database.module'
 import { StudentsModule } from './students/students.module'
 import { EmailModule } from './email/email.module'
 import { ArticleModule } from './article/article.module'
@@ -14,7 +14,7 @@ require('dotenv')
 
 @Module({
   imports: [
-    DatabaseModule,
+    //DatabaseModule,
     ConfigModule.forRoot(),
     StudentsModule,
     EmailModule,
