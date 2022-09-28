77cb609e04b105ed1d0142a89e870b9ec17a8b1a
Gustavo Oliveira Quinteiro <gustavo.quinteiro@ufba.br>
2022-09-28 12:25:18 -0300

    add new search in advisor table to validate user login

--- src/user/service/user.service.ts
+++ src/user/service/user.service.ts
@@ -4,17 +4,28 @@ import { Repository } from 'typeorm'
 import { CreateUserDTO } from '../model/user.dto.input'
 import { NotFoundException } from '@nestjs/common'
 import { errorMessages } from '../../auth/constants'
+import { Advisor } from '../../advisor/entities/advisor.entity'
 
 export class UserService {
-  constructor(
-    @InjectRepository(StudentEntity)
-    private studentRepository: Repository<StudentEntity>
-  ) {}
+  @InjectRepository(StudentEntity)
+  private studentRepository: Repository<StudentEntity>
+  @InjectRepository(Advisor)
+  private advisorRepository: Repository<Advisor>
+  constructor() {
+  }
 
   async findUserByTaxId(tax_id: string): Promise<CreateUserDTO> {
     try {
-      const user = await this.studentRepository.findOne({ where: { tax_id } })
+      let user = null
+      const studentUser = await this.studentRepository.findOne({ where: { tax_id } })
+      const advisorUser = await this.advisorRepository.findOne({ where: { tax_id } })
+      if (advisorUser){ 
+        user = advisorUser
+      }else if (studentUser){
+        user = studentUser
+      }
       if (user) return new CreateUserDTO(user)
+      throw new NotFoundException(errorMessages.userNotFoundMessage)
     } catch (error) {
       throw new NotFoundException(errorMessages.userNotFoundMessage)
     }
