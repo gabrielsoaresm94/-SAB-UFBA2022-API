7016299cb031e223c8dbfedc4bad06ba837c7427
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2022-11-21 09:01:29 -0300

    add scholarship search by agency

--- src/scholarship/service/scholarship.service.ts
+++ src/scholarship/service/scholarship.service.ts
@@ -14,7 +14,7 @@ import { Scholarship, toScholarshipDTO } from '../entities/scholarship.entity'
 export class ScholarshipService {
   constructor(
     @InjectRepository(Scholarship)
-    private scholarshipRepository: Repository<Scholarship>
+    private scholarshipRepository: Repository<Scholarship>,
   ) {}
 
   async create(createScholarshipDto: CreateScholarshipDto) {
@@ -68,19 +68,27 @@ export class ScholarshipService {
       { scholarship_ends_at: newFinalDate.newFinalDate }
     )
   }
-  async findByAgencyAndModel(id_agency: number, model: string) {
+  async findByAgencyAndModel(agency_id: number, model: string) {
     if (!model) {
       const scholarships = await this.scholarshipRepository.find({
-        relations: { agency: true },
-        where: { agency_id: id_agency }
+        relations: { agency: true, student: true },
+        where: { agency_id: agency_id }
       })
       return scholarships.map((scholarship) => toScholarshipDTO(scholarship))
     } else {
       const scholarships = await this.scholarshipRepository.find({
         relations: { agency: true },
-        where: { agency_id: id_agency, model: model }
+        where: { agency_id: agency_id, model: model }
       })
       return scholarships.map((scholarship) => toScholarshipDTO(scholarship))
     }
+  }
+
+  async findByAgency(agency_id: number, active: boolean) {
+    const scholarships = await this.scholarshipRepository.find({
+      relations: { agency: true },
+      where: { agency_id: agency_id, active: active }
+    })
+    return scholarships.map((scholarship) => toScholarshipDTO(scholarship))
   }
 }
